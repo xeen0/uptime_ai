e97608c4c85028730d868ef3f9d491fff4c26ad4
xeen0 <sahitsrivyshnav1999@gmail.com>
2025-12-01 21:19:10 +0530

    refactor: adjust layout styles in OverviewPage for better responsiveness

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -5,7 +5,7 @@ import RightPanelComponent from "../components/RightPanelComponent";
 export default function OverviewPage({ user }) {
   if (!user) return null;
   return (
- <div className="flex flex-col md:flex-row gap-8 h-screen">
+    <div className="flex flex-col md:flex-row gap-8">
       <div className="md:w-72 w-full flex-none">
         <div className="h-full overflow-y-auto">
           <LeftPanelComponent user={user} />
@@ -13,7 +13,7 @@ export default function OverviewPage({ user }) {
       </div>
 
       <div className="flex-1 min-w-0 flex flex-col">
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex-1 overflow-y-auto max-h-[calc(100vh-180px)]">
           <RightPanelComponent user={user} />
         </div>
       </div>
